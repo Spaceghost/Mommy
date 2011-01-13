3813aee709a40771f90aaca170450da3949b8b8d
Johnneylee Jack Rollins <Johnneylee.Rollins@gmail.com>
2011-01-13 00:33:50 -0800

    Added cucumber and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mommy::Application.config.secret_token = 'f7444b687999480d8c4a3c852cedfb98956400cf19fc8588fc1befb08bf32c694444f127fe491fe7e1d6639a60bb844c75e1dfb74286765b54efb42b60490547'
